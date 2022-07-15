9d348e597573329f5d3724af98fc18370d355d8f
Z-tech2 <107017335+Z-tech2@users.noreply.github.com>
2022-07-15 18:03:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #Read README before this to learn how to use it
 def ReplaceChar(var, pos, replacement):
+  CharType = type(var);
+  var = str(var)
   var = list(var);
   var[pos] = replacement;
-  var = str(var);
+  var = CharType(var);
   return var;
